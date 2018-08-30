34e4e04a8f1690e7165e52d4549f42337a868535
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-08-30 14:49:30 -0500

    added login redirects

--- chapter1/intro-app/src/components/login/Login.js
+++ chapter1/intro-app/src/components/login/Login.js
@@ -32,6 +32,7 @@ export default class Login extends Component {
                 password: this.state.password
             })
         )
+        this.props.history.push("/")
     }
 
     render() {
